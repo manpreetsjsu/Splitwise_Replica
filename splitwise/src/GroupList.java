c2ffea6eda4c5a4079cc6654d212b6c71f4f9b75
Manpreet Singh <manpreetsingh_er@yahoo.com>
2019-11-13 13:41:52 -0800

    dynamic deleteGroup,deleteMember

--- splitwise/src/GroupList.java
+++ splitwise/src/GroupList.java
@@ -1,16 +1,26 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 
 public class GroupList {
-	private ArrayList<Group> groupList;
+	private HashMap<Integer,Group> groupList;
 	
 	public GroupList() {
-		groupList = new ArrayList<>();
+		groupList = new HashMap<>();
 	}
 	public void addGroup(Group newGroup) {
-		groupList.add(0,newGroup);
+		// if same group name exists, just return
+		if(groupList.containsKey(newGroup.hashCode())){
+			return;
+		}
+		groupList.put(newGroup.getName().hashCode(),newGroup);
 	}
 	
-	public ArrayList<Group> getGroupList() {
-		return this.groupList;
+	public Collection<Group> getGroupList() {
+		return this.groupList.values();
+	}
+	
+	public void deleteGroup(int key) {
+		groupList.remove(key);
 	}
 }
