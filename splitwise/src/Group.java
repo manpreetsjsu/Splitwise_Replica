f8cec6e629222b1cdded6796f431302b50952c5d
Manpreet Singh <manpreetsingh_er@yahoo.com>
2019-11-19 01:38:08 -0800

    Add group simplifyDebts algorithm

--- splitwise/src/Group.java
+++ splitwise/src/Group.java
@@ -1,37 +1,87 @@
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Group {
-	
+
 	private String name;
-	//private ArrayList<Member> memberList ;
-	private HashMap<Integer,Member> memberList ;
-	
+	// private ArrayList<Member> memberList ;
+	private HashMap<Integer, Member> memberList;
+
 	public Group(String name) {
 		this.name = name;
-		memberList= new HashMap<>();
-	
+		memberList = new HashMap<>();
+
 	}
-	
+
 	public String getName() {
-		return this.name ;
+		return this.name;
 	}
-	
+
 	public void setName(String newName) {
 		this.name = newName;
 	}
+
 	public void addMember(Member m) {
-		if(memberList.containsKey(m.getName().hashCode())) return ;
+		if (memberList.containsKey(m.getName().hashCode()))
+			return;
 		memberList.putIfAbsent(m.getName().hashCode(), m);
 	}
-	
-	public Collection<Member> getMembers(){
+
+	public Collection<Member> getMembers() {
 		return this.memberList.values();
 	}
-	
+
+	public HashMap<Integer, Member> getMemberListMap() {
+		return this.memberList;
+	}
+
 	public void deleteMember(int key) {
 		memberList.remove(key);
 	}
-	
+
+	/*
+	 * This function combine and simplify debts of Each member of the group
+	 */
+	public void simplifyGroupDebts() {
+
+		for (Member debter : this.getMembers()) {
+
+			for (Map.Entry<Member, Double> lender : debter.getOweList().entrySet()) {
+				// get moneyOwed Bob [debter] -> Alice [lender]
+				Member lenderPerson = lender.getKey();
+				double debterOwesToLender = lender.getValue();
+				// get moneyOwed Alice [lender] -> Bob [debter]
+				double lenderOwesToDebter = lenderPerson.getOweList().get(debter) != null
+						? lenderPerson.getOweList().get(debter)
+						: 0;
+
+				if (lenderOwesToDebter > debterOwesToLender) {
+					lenderPerson.modifySimplfiedDebtList(debter, lenderOwesToDebter - debterOwesToLender);
+					// System.out.println(lenderPerson.getName() + " -> " + debter.getName() + " ->
+					// " + (lenderOwesToDebter - debterOwesToLender));
+					debter.modifySimplfiedDebtList(lenderPerson, 0);
+					// System.out.println(debter.getName() + " -> " + lenderPerson.getName() + " ->
+					// " + 0);
+
+				} else if (lenderOwesToDebter < debterOwesToLender) {
+					debter.modifySimplfiedDebtList(lenderPerson, debterOwesToLender - lenderOwesToDebter);
+					// System.out.println(debter.getName() + " -> " + lenderPerson.getName() + " ->
+					// " + (debterOwesToLender - lenderOwesToDebter));
+					lenderPerson.modifySimplfiedDebtList(debter, 0);
+					// System.out.println(lenderPerson.getName() + " -> " + debter.getName() + " ->
+					// " + 0);
+				}
+
+				else if (lenderOwesToDebter == debterOwesToLender) {
+					// System.out.println("Balancing Out balances");
+					debter.modifySimplfiedDebtList(lenderPerson, 0);
+					lenderPerson.modifySimplfiedDebtList(debter, 0);
+				}
+
+			}
+		}
+	}
+
 }
--- splitwise/src/Member.java
+++ splitwise/src/Member.java
@@ -1,35 +1,60 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Member {
-	
-	private String name ;
-	private float balance ;
-	private ArrayList<Transaction> activities ;
-	
-	
+
+	private String name;
+	private ArrayList<Transaction> activities;
+	private HashMap<Member, Double> oweList; // this is list of members which this Member owes to them
+
+	// this list is the final list that this member owes the other members which are
+	// keys of hashmap
+	private HashMap<Member, Double> simplifiedDebtList;
+
 	public Member(String name) {
-		this.name = name ;
+		this.name = name;
 		activities = new ArrayList<>();
+		oweList = new HashMap<Member, Double>();
+		simplifiedDebtList = new HashMap<Member, Double>();
 	}
+
 	public ArrayList<Transaction> getActivities() {
-		return this.activities ;
+		return this.activities;
 	}
-	
+
 	public void addActivity(Transaction e) {
 		this.activities.add(e);
 	}
-	
-	public void addToBalance(float amt) {
-		this.balance += amt;
+
+	public void modifyOweList(Member m, double oweAmount) {
+
+		if (oweList.containsKey(m)) {
+			oweList.put(m, oweList.get(m) + oweAmount);
+			return;
+		}
+		oweList.putIfAbsent(m, oweAmount);
 	}
-	
-	public void reduceToBalance(float amt) {
-		this.balance -= amt;
-	
+
+	public void modifySimplfiedDebtList(Member m, double oweAmount) {
+
+		if (this.simplifiedDebtList.containsKey(m)) {
+			this.simplifiedDebtList.put(m, oweAmount);
+			return;
+		}
+
+		simplifiedDebtList.putIfAbsent(m, oweAmount);
 	}
-	
+
+	public HashMap<Member, Double> getOweList() {
+		return this.oweList;
+	}
+
+	public HashMap<Member, Double> getSimplifiedDebtList() {
+		return this.simplifiedDebtList;
+	}
+
 	public String getName() {
 		return this.name;
 	}
-	
+
 }
